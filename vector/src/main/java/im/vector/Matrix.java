032cbc25e903e1f2bd064a1919becfc566f83554
Valere <valeref@matrix.org>
2019-01-22 09:20:47 +0100

    Cleaning and refactoring

--- vector/src/main/java/im/vector/Matrix.java
+++ vector/src/main/java/im/vector/Matrix.java
@@ -171,7 +171,7 @@ public class Matrix {
             mRefreshUnreadCounter = false;
 
             Log.d(LOG_TAG, "onLiveEventsChunkProcessed ");
-            EventStreamService.checkDisplayedNotifications();
+            //EventStreamService.checkDisplayedNotifications();
         }
     };
 
