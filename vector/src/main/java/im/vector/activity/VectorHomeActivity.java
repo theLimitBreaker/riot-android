80276a4ed590a4478541df82ec284cae510e540e
Valere <valeref@matrix.org>
2019-02-07 11:15:59 +0100

    Revert permission battery
    cleaning

--- vector/src/main/java/im/vector/activity/VectorHomeActivity.java
+++ vector/src/main/java/im/vector/activity/VectorHomeActivity.java
@@ -687,7 +687,7 @@ public class VectorHomeActivity extends VectorAppCompatActivity implements Searc
                 return;
             }
 
-            if (!PreferencesManager.didAskUserToIgnoreBatteryOptimizations(this)) {
+            if (pushManager.isBackgroundSyncAllowed() && !PreferencesManager.didAskUserToIgnoreBatteryOptimizations(this)) {
                 PreferencesManager.setDidAskUserToIgnoreBatteryOptimizations(this);
 
                 if (!SystemUtilsKt.isIgnoringBatteryOptimizations(this)) {
